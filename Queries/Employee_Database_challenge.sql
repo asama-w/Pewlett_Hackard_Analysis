5838782ff2455e0aaa1955bb0d1c889ef77a4b07
Asama Wongbusarakhum <wiwiiiii@Wiwiis-MacBook-Pro.local>
2022-04-24 07:07:36 -0400

    Adding Challenge Materials

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,53 @@
+-- Deliverable 1
+
+-- Create Retirement titles table 
+-- (List of retirement-eligible employees with titles)
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Create Unique title table (Most recent title)
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+
+-- Number of retirees by their most recent job title
+SELECT COUNT(emp_no) AS count, title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+-- Deliverable 2
+-- Create a Mentorship Eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO membership_eligibility
+FROM employees AS e
+INNER JOIN dept_employee AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no
